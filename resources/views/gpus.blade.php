00f77beb718ba554a26ede61602e0b9d79baef51
AtanasNaydenov <AtanasNaydenov@outlook.com>
2016-09-14 17:08:49 +0200

    Button More functionality implemented

--- resources/views/gpus.blade.php
+++ resources/views/gpus.blade.php
@@ -53,7 +53,7 @@
                             <img src="{{ URL::to('/') }}/images/nvidia.png" class="img-rounded image-gpu" alt="nvidia logo">
                         </div>
                         <div class="more-button-gpu col-md-4">
-                            <button class="btn btn-info">MORE</button>
+                            <button class="btn btn-info" id="myBtn{{$gpu->id}}">MORE</button>
                         </div>
                     </div>
                     <div class="row">
@@ -70,6 +70,16 @@
                 </div>
             </div>
         </row>
+            <!-- The Modal -->
+            <div id="myModal{{$gpu->id}}" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="close">x</span>
+                    <p>{{$gpu->name}}</p>
+                </div>
+
+            </div>
         @endforeach
 
 
@@ -77,6 +87,39 @@
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>
+    @foreach($gpus as $gpu)
+    // Get the modal
+    var modal{{$gpu->id}} = document.getElementById('myModal{{$gpu->id}}');
+
+    // Get the button that opens the modal
+    var btn{{$gpu->id}} = document.getElementById("myBtn{{$gpu->id}}");
+
+    // When the user clicks on the button, open the modal
+    btn{{$gpu->id}}.onclick = function() {
+        modal{{$gpu->id}}.style.display = "block";
+    }
+
+    // Get the <span> element that closes the modal
+    var spanNumber{{$gpu->id}} = {{$gpu->id}} - 2;
+    var span{{$gpu->id}} = document.getElementsByClassName("close")[spanNumber{{$gpu->id}}];
+
+    // When the user clicks on <span> (x), close the modal
+    span{{$gpu->id}}.onclick = function(event) {
+        modal{{$gpu->id}}.style.display = "none";
+
+    }
+        @endforeach
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        @foreach($gpus as $gpu)
+        if (event.target == modal{{$gpu->id}}) {
+            modal{{$gpu->id}}.style.display = "none";
+        }
+        @endforeach
+    }
+</script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
