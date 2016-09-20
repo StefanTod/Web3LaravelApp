14290311f74fd8d3e46bda4816fea9240509ff05
AtanasNaydenov <AtanasNaydenov@outlook.com>
2016-09-20 22:58:53 +0200

    comment front end

---
+++ resources/views/comment.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Comment Name</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/comment.css">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-default ">
+        <div class="container-fluid col-md-11 col-md-offset-1">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">GPU INTELLIGENCE</a>
+            </div>
+
+
+        </div><!-- /.container-fluid -->
+    </nav>
+</header>
+
+<div id="respond">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9 col-sm-offset-3 ">
+                <h3>Comments for *Insert name here*</h3>
+            </div><!-- /col-sm-12 -->
+        </div><!-- /row -->
+        <br>
+
+        <!-- foreach starts here -->
+        <div class="row">
+            <div class="col-sm-1 col-sm-offset-3">
+                <div class="thumbnail">
+                    <img class="img-responsive user-photo" src="images/avatar.png">
+                </div><!-- /thumbnail -->
+            </div><!-- /col-sm-1 -->
+
+            <div class="col-sm-5">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <strong>getname</strong> <span class="text-muted">if we have time</span>
+                    </div>
+                    <div class="panel-body">
+                        get text
+                    </div><!-- /panel-body -->
+                </div><!-- /panel panel-default -->
+            </div><!-- /col-sm-5 -->
+        </div><!-- /row -->
+        <!-- foreach ends here-->
+    </div><!-- /container -->
+</div>
+</body>
+</html>
